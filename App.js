6831be37b6718afe7c69a4b16795298b53390855
mike242424 <mikebyers24@gmail.com>
2024-03-27 16:58:15 -0400

    added focus method for secret number input

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { TextInput } from 'react-native';
+import { Text, TextInput } from 'react-native';
 import { style } from './App.style';
+import { useRef } from 'react';
 
 const App = () => {
+  const secretCodeRef = useRef();
+  const expirationDateRef = useRef();
+
+  function onCardNumberChange(text) {
+    if (text.length > 16) {
+      secretCodeRef.current.focus();
+    }
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={style.container}>
-        <TextInput style={style.text} placeholder="Card Number" />
-        <TextInput style={style.text} placeholder="Secret Code" />
-        <TextInput style={style.text} placeholder="Expiration Date" />
+        <TextInput
+          onChangeText={onCardNumberChange}
+          style={style.text}
+          placeholder="Card Number"
+        />
+        <TextInput
+          ref={secretCodeRef}
+          style={style.text}
+          placeholder="Secret Code"
+        />
+        <TextInput
+          ref={expirationDateRef}
+          style={style.text}
+          placeholder="Expiration Date"
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
